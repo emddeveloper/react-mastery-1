ff0abf789661b50c7f5ca5fe10eb548d8b1b9215
emddeveloper <emd.developer@gmail.com>
2020-08-22 20:27:16 +0530

    semantic css added and searchBar component created

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Search Bard</div>
+      </div>
+    )
+  }
+}
+
+export default SearchBar
