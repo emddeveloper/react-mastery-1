e374cf3285e00ccdfc405fc9d3a257f6a40a3c39
emddeveloper <emd.developer@gmail.com>
2020-08-22 20:51:01 +0530

    search term is ready

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,23 @@
 import React from "react"
 
 class SearchBar extends React.Component {
+  state = { term: "" }
+  onInutChange = e => {
+    this.setState({ term: e.target.value })
+  }
+  onFormSubmit = e => {
+    e.preventDefault()
+    // TODO make sure we call callback from parent component
+  }
   render() {
     return (
-      <div>
-        <div>Search Bard</div>
+      <div className="ui segment search-bar">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label htmlFor="search">Video Search</label>
+            <input type="text" id="search" value={this.state.term} onChange={this.onInutChange} />
+          </div>
+        </form>
       </div>
     )
   }
