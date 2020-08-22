9bfd05ff8ef2432c755e7347bc420b9472ff3235
emddeveloper <emd.developer@gmail.com>
2020-08-22 21:23:13 +0530

    youtube api call successful

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from "react"
 import SearchBar from "./SearchBar"
+import youtube from "../api/youtube"
 
 class App extends React.Component {
+  state = { videos: [] }
+  ontermSubmit = async term => {
+    const respnse = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    })
+    this.setState({ videos: respnse.data.items })
+  }
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.ontermSubmit} />
       </div>
     )
   }
